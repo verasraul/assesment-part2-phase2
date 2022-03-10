3cf76bf5463473785ba9bea02d83a1df88356ff2
Raul Veras <verasraul@gmail.com>
2022-03-09 21:50:48 -0500

    updating files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import GetCats from './components/GetCats'
+import GetDogs from './components/GetDogs'
+
+
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className='links'>
+        {/* 'NavLink' links your pages. */}
+        <nav>
+          <NavLink to={"/"} >Home</NavLink>
+          <NavLink to={"GetCats"} >GetCats</NavLink>
+          <NavLink to={"GetDogs"} >GetDogs</NavLink>
+        </nav>  
+      </div>
+
+      <div>
+      {/* // 'Routes' renders your pages. */}
+      <Routes>
+      // 'Route' in singular form is a self-closing tag.
+        <Route path='/' element={<Home /> } />
+        <Route path='GetCats' element={<GetCats /> } />
+        <Route path='GetDogs' element={<GetDogs /> } />
+      </Routes>
+     
+      </div>
+
+
     </div>
   );
 }
